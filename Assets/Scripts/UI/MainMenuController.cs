aa05c4acf2a0661cfc8a9a482b430a10893e4fbf
Vitor Rodrigues <oldbard@gmail.com>
2021-01-30 15:12:06 +0100

    Enables MainMenu button.

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnStartGame()
+    {
+        SceneManager.LoadScene("GameplayScene");
+    }
+}
